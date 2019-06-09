80267d13aef531c302d9d73c1beca956072b41c3
skullnight1991 <48285226+skullnight1991@users.noreply.github.com>
2019-06-09 15:12:36 +0700

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -3,9 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-data = pd.read_csv('data_linear.csv').values
-x = np.array(data[:, 0]).reshape([-1,1])
-y = np.array(data[:, 1]).reshape([-1,1])
+data = pd.read_csv('Advertising.csv').values
+x = np.array(data[:, 2])
+y = np.array(data[:, 4])
 
 def forward(x):
 	return x * w
@@ -14,29 +14,32 @@ def loss(x, y):
 	y_pred = forward(x)
 	return (y_pred - y) * (y_pred - y)
 
-l_func = np.zeros((20))
+l_list = []
 w_list = []
-w = 1
-for i in range(20):
+w = 0
+for i in range(200):
     # r = y^ - y
-    r = np.dot(x, w) - y
-    print(np.sum(r*r))
+    r = (x * w) - y
     # Calculate loss function
-    l_func[i] = 0.5 * np.sum(r*r) / len(x)
+    l_func = 0.5 * np.sum(r * r) / len(x)
+    #print(l_func)
     
     # Update the weight
-    w -= 0.0000001 * np.sum(np.multiply(x, 2) * (np.sum(r)))
+    w -= 0.000000001 * 2 * np.sum(x) * (np.sum(r))
 
+    l_list.append(l_func)
     w_list.append(w)
 		
 print('%s %f' % ('Min weight=', min(w_list)))
 print('%s %f' % ('Min loss=',l_func.min()))
+print(w_list)
 #print(l_list)
 
 minIndex = np.argmin(x)
 maxIndex = np.argmax(x)
 
 pred = np.dot(x, w)
-plt.scatter(x,y)
-plt.plot([x[minIndex], x[maxIndex]], [pred[minIndex], pred[maxIndex]] , 'r') 
+plt.plot(w_list, l_list)
+#plt.scatter(x,y)
+#plt.plot([x[minIndex], x[maxIndex]], [pred[minIndex], pred[maxIndex]] , 'r') 
 plt.show()
